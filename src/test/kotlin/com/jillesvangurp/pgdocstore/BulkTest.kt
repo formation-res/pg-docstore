58856fff3783f8ce7dadec7ed8ecf71122beab5e
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2023-12-19 12:46:22 +0100

    test bulk updates

--- src/test/kotlin/com/jillesvangurp/pgdocstore/BulkTest.kt
+++ src/test/kotlin/com/jillesvangurp/pgdocstore/BulkTest.kt
@@ -3,6 +3,7 @@ package com.jillesvangurp.pgdocstore
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.toList
 import org.junit.jupiter.api.Test
 import java.util.*
 import kotlin.random.Random
@@ -14,12 +15,17 @@ class BulkTest : DbTestBase() {
     fun shouldBulkInsert() = coRun {
         val docFlow = flow {
             repeat(200) {
-                emit(UUID.randomUUID().toString() to TestModel(Random.nextULong().toString()))
+                emit(TestModelWithId(Random.nextULong().toString()))
             }
         }
-        val ds = DocStore(db, TestModel.serializer(), tableName)
-        ds.bulkInsertWithId(docFlow, chunkSize = 12)
+        val ds = DocStore(db, TestModelWithId.serializer(), tableName)
+        ds.bulkInsert(docFlow, chunkSize = 12)
         ds.count() shouldBe 200
-        ds.documentsByRecency().count() shouldBe 200
+        val stored = ds.documentsByRecency().toList()
+        stored.size shouldBe 200
+        stored.map { it.copy(property = it.property.reversed()) }.let {
+            ds.bulkInsert(it)
+        }
+        ds.count() shouldBe 200
     }
 }
