f725a7076807aaac68a4384cfd14b175108f17a7
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2024-01-05 20:02:10 +0100

    get rid of inTransaction so it doesn't roll back table creation and fail

--- src/main/kotlin/com/tryformation/pgdocstore/schema-support.kt
+++ src/main/kotlin/com/tryformation/pgdocstore/schema-support.kt
@@ -4,9 +4,8 @@ import com.github.jasync.sql.db.SuspendingConnection
 
 
 suspend fun SuspendingConnection.dropDocStoreTable(tableName: String = "docstore") {
-    inTransaction { c ->
-        c.sendQuery(
-            """
+    sendQuery(
+        """
                 drop index if exists idx_${tableName}_created_at;
                 drop index if exists idx_${tableName}_updated_at;
                 drop index if exists idx_${tableName}_tags;
@@ -14,38 +13,35 @@ suspend fun SuspendingConnection.dropDocStoreTable(tableName: String = "docstore
                 
                 drop table if exists $tableName;
             """.trimIndent()
-        )
-    }
+    )
 }
 
 suspend fun SuspendingConnection.createDocStoreTable(tableName: String) {
-    inTransaction { c ->
-        runCatching {
-            // this sometimes fails in CI and we don't care
-            // we have lots of tests running concurrently so this can happen
-            // somehow
-            c.sendQuery(
-                "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
-            )
-        }
-        c.sendQuery(
-            """
-                CREATE TABLE IF NOT EXISTS "$tableName" (
-                  id       text PRIMARY KEY,
-                  created_at timestamptz DEFAULT current_timestamp,
-                  updated_at timestamptz DEFAULT current_timestamp,            
-                  json     text,
-                  tags     text[],
-                  text     text
-                );
-                
-                CREATE INDEX IF NOT EXISTS idx_${tableName}_created_at ON ${tableName} (created_at);
-                CREATE INDEX IF NOT EXISTS idx_${tableName}_updated_at ON ${tableName} (updated_at);
-                CREATE INDEX IF NOT EXISTS idx_${tableName}_tags ON ${tableName} USING gin (tags);
-                CREATE INDEX IF NOT EXISTS idx_${tableName}_text ON ${tableName} USING gin (text gin_trgm_ops);
-            """.trimIndent()
+    runCatching {
+        // this sometimes fails in CI and we don't care
+        // we have lots of tests running concurrently so this can happen
+        // somehow
+        sendQuery(
+            "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
         )
     }
+    sendQuery(
+        """
+            CREATE TABLE IF NOT EXISTS "$tableName" (
+              id       text PRIMARY KEY,
+              created_at timestamptz DEFAULT current_timestamp,
+              updated_at timestamptz DEFAULT current_timestamp,            
+              json     text,
+              tags     text[],
+              text     text
+            );
+            
+            CREATE INDEX IF NOT EXISTS idx_${tableName}_created_at ON ${tableName} (created_at);
+            CREATE INDEX IF NOT EXISTS idx_${tableName}_updated_at ON ${tableName} (updated_at);
+            CREATE INDEX IF NOT EXISTS idx_${tableName}_tags ON ${tableName} USING gin (tags);
+            CREATE INDEX IF NOT EXISTS idx_${tableName}_text ON ${tableName} USING gin (text gin_trgm_ops);
+        """.trimIndent()
+    )
 }
 
 suspend fun SuspendingConnection.reCreateDocStoreTable(tableName: String) {
